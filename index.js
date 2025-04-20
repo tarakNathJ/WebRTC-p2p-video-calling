977edc533e496626e4a57364da1e4b4e54523caf
tarak <janatarak739@gmail.com>
2025-04-21 02:16:03 +0530

    webRtc Backend

---
+++ index.js
@@ -0,0 +1,48 @@
+import { WebSocket, WebSocketServer } from 'ws';
+import express from 'express';
+const app = express();
+const server = app.listen(8080, () => {
+  console.log("server start 4000 port")
+});
+
+const wss = new WebSocketServer({ server: server });
+
+let senderSocket = null;
+let receiverSocket = null;
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    const message = JSON.parse(data);
+    if (message.type === 'sender') {
+      senderSocket = ws;
+    }
+    else if (message.type === 'receiver') {
+      receiverSocket = ws;
+    }
+    else if (message.type === 'createOffer') {
+      if (ws === senderSocket) {
+        receiverSocket?.send(JSON.stringify({ type: 'createOffer', sdp: message.sdp }));
+      } else {
+        senderSocket?.send(JSON.stringify({ type: 'createOffer', sdp: message.sdp }));
+        
+      }
+    }
+    else if (message.type === 'createAnswer') {
+      if (ws === receiverSocket) {
+        senderSocket?.send(JSON.stringify({ type: 'createAnswer', sdp: message.sdp }));
+      }else{
+        receiverSocket?.send(JSON.stringify({ type: 'createAnswer', sdp: message.sdp }));
+      }
+      
+    }
+    else if (message.type === 'iceCandidate') {
+      if (ws === senderSocket) {
+        receiverSocket?.send(JSON.stringify({ type: 'iceCandidate', candidate: message.candidate }));
+      } else if (ws === receiverSocket) {
+        senderSocket?.send(JSON.stringify({ type: 'iceCandidate', candidate: message.candidate }));
+      }
+    }
+  });
+});
